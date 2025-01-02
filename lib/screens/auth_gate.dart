a46df4dffa825b4e14ec42e5285ec5afeec20831
Malik-Ali-482 <231555@students.au.edu.pk>
2025-01-02 14:57:20 +0500

    2 jan

--- lib/screens/auth_gate.dart
+++ lib/screens/auth_gate.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:recipie_app/screens/app_main_screen.dart';
 import 'package:recipie_app/screens/auth_screen.dart';
+import 'package:recipie_app/Provider/favourite_provider.dart'; // Adjust the path as needed
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
@@ -9,17 +11,27 @@ class AuthGate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if(snapshot.hasData){
-              return const AppMainScreen();
-            }
-            else{
-              return const AuthScreen();
-            }
-          },
-        )
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final user = snapshot.data;
+            return ChangeNotifierProvider(
+              create: (_) {
+                final favoriteProvider = FavoriteProvider();
+                if (user != null) {
+                  favoriteProvider.setUserId(user.uid);
+                }
+                return favoriteProvider;
+              },
+              child: const AppMainScreen(),
+            );
+          } else {
+            // User is not logged in
+            return const AuthScreen();
+          }
+        },
+      ),
     );
   }
 }
